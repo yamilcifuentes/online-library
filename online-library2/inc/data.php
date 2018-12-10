db18b35731a3990907bbe28465f45dd75d205a7c
yamilcifuentes <40443521+yamilcifuentes@users.noreply.github.com>
2018-12-10 18:34:46 -0500

    Add files via upload

---
+++ online-library2/inc/data.php
@@ -0,0 +1,183 @@
+<?php
+$catalog = [];
+//Books
+$catalog[101] = [
+	"title" => "Crazy Is My Superpower: How I Triumphed by Breaking Bones, Breaking Hearts, and Breaking the Rules",
+	"img" => "img/media/ajlee.jpg",
+    "genre" => "Auto Biography",
+    "format" => "Hard Cover",
+    "year" => 2017,
+    "category" => "Books",
+    "authors" => [
+        "AJ Mendez Brooks",
+
+    ],
+    "publisher" => "Crown/Archetype",
+    "isbn" => '9780451496669'
+];
+$catalog[102] = [
+    "title" => "No Is A Four Letter Word: How I Failed Spelling but Succeeded in Life",
+    "img" => "img/media/jericho.jpg",
+    "genre" => "Auto Biography",
+    "format" => "Hard Cover",
+    "year" => 2017,
+    "category" => "Books",
+    "authors" => [
+        "Chris Jericho"
+    ],
+    "publisher" => "Da Capo Press",
+    "isbn" => '9780306825057'
+];
+$catalog[103] = [
+    "title" => "Slobberknocker: My Life in Wrestling
+",
+    "img" => "img/media/ross.jpg",
+    "genre" => "Auto Biography",
+    "format" => "Hard cover",
+    "year" => 2017,
+    "category" => "Books",
+    "authors" => [
+        "Jim Ross"
+
+    ],
+    "publisher" => "Sports Publishing LLC",
+    "isbn" => '9781683581130'
+];
+$catalog[104] = [
+    "title" => "Saint Mick: My Journey From Hardcore Legend to Santa's Jolly Elf",
+    "img" => "img/media/mick.jpg",
+    "genre" => "Auto Biography",
+    "format" => "Hard Cover",
+    "year" => 2017,
+    "category" => "Books",
+    "authors" => [
+        "Mick Foley"
+    ],
+    "publisher" => "Polis Books",
+    "isbn" => '9781947993365'
+];
+//Movies
+$catalog[201] = [
+    "title" => "Spider-Man",
+    "img" => "img/media/spiderman1.jpg",
+    "genre" => "Action",
+    "format" => "DVD",
+    "year" => 2002,
+    "category" => "Movies",
+    "director" => "Sam Raimi",
+    "writers" => [
+        "David Koepp"
+
+    ],
+    "stars" => [
+        "Tobey Maguire",
+        "William Defoe",
+        "Kirsten Dunst",
+        "James Franco"
+    ]
+];
+$catalog[202] = [
+    "title" => "Spider-Man 2",
+    "img" => "img/media/spiderman2.jpg",
+    "genre" => "Action",
+    "format" => "Blu-ray",
+    "year" => 2004,
+    "category" => "Movies",
+    "director" => "Sam Raimi",
+    "writers" => [
+        "Alvin Sargent"
+    ],
+    "stars" => [
+        "Tobey Maguire",
+        "Alfred Molina",
+        "Kirsten Dunst",
+        "James Franco",
+
+    ]
+];
+$catalog[203] = [
+    "title" => "Spider-Man: Homecoming",
+    "img" => "img/media/SpiderManHomecoming.jpg",
+    "genre" => "Action",
+    "format" => "Blu-ray",
+    "year" => 2017,
+    "category" => "Movies",
+    "director" => "Jon Watts",
+    "writers" => [
+        "Jon Watts",
+        "John Francis Daley",
+        "Jonathan Goldstein",
+        "Chris McKenna",
+				"Erik Sommers",
+				"Christopher D. Ford"
+    ],
+    "stars" => [
+        "Tom Holland",
+        "Michael Keaton",
+        "Robert Downey Jr.",
+        "Laura Harrier",
+        "Jacob Batalon",
+        "Zendaya"
+    ]
+];
+$catalog[204] = [
+    "title" => "The Avengers",
+    "img" => "img/media/TheAvengers.jpg",
+    "genre" => "Action",
+    "format" => "Blu-ray",
+    "year" => 2012,
+    "category" => "Movies",
+    "director" => "Joss Whedon",
+									"Anthony Russo",
+									"Joe Russo",
+    "writers" => [
+        "Joss Whedon"
+    ],
+    "stars" => [
+        "Robert Downey Jr.",
+        "Chris Evans",
+        "Mark Ruffalo",
+        "Chris Hemsworth",
+        "Tom Hiddleston",
+        "Samuel L. Jackson",
+        "Scarlett Johansson",
+
+    ]
+];
+//Music
+$catalog[301] = [
+    "title" => "Hysteria",
+    "img" => "img/media/def.jpg",
+    "genre" => "Hard Rock",
+    "format" => "CD",
+    "year" => 1987,
+    "category" => "Music",
+    "artist" => "Def Leppard"
+];
+$catalog[302] = [
+    "title" => "Judas",
+    "img" => "img/media/Fozzy.jpg",
+    "genre" => "Pop",
+    "format" => "CD",
+    "year" => 2017,
+    "category" => "Music",
+    "artist" => "Fozzy"
+];
+$catalog[303] = [
+    "title" => "The Catalyst",
+    "img" => "img/media/linkinpark.jpg",
+    "genre" => "Hard Rock",
+    "format" => "CD",
+    "year" => 2010,
+    "category" => "Music",
+    "artist" => "Linkin Park"
+];
+$catalog[304] = [
+    "title" => "Come And Get It",
+    "img" => "img/media/prevail.jpg",
+    "genre" => "Punk",
+    "format" => "CD",
+    "year" => 2016,
+    "category" => "Music",
+    "artist" => "I Prevail"
+];
